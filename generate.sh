870661841e46caa891088ef39ccd8c2a49623988
suresh07 <soori_rozee@yahoo.com>
2015-04-02 17:03:09 +0530

    Commit No. 11 - generate.sh changed - db and pwd parametrized

--- generate.sh
+++ generate.sh
@@ -3,9 +3,9 @@
 host="localhost"
 db="tattvaloka"
 usr="root"
-pwd="mysql"
+pwd='mysql'
 
-echo "drop database if exists tattvaloka; create database tattvaloka charset utf8 collate utf8_general_ci;" | /usr/bin/mysql -uroot -pmysql
+echo "drop database if exists $db; create database $db charset utf8 collate utf8_general_ci;" | /usr/bin/mysql -uroot -p$pwd
 
 perl insert_author.pl $host $db $usr $pwd
 perl insert_feat.pl $host $db $usr $pwd
@@ -13,6 +13,4 @@ perl insert_articles.pl $host $db $usr $pwd
 perl ocr.pl $host $db $usr $pwd
 perl searchtable.pl $host $db $usr $pwd
 
-echo "create fulltext index text_index on searchtable (text);" | /usr/bin/mysql -uroot -pmysql $db
-
-
+echo "create fulltext index text_index on searchtable (text);" | /usr/bin/mysql -uroot -p$pwd $db
