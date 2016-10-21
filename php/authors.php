4468dad0b222d303ae042bde184a56a0aff254d8
Shruthisdst <shruthitr.nayak@gmail.com>
2016-10-21 19:06:45 +0530

    Author names are displying in alphabetical order

--- php/authors.php
+++ php/authors.php
@@ -3,7 +3,7 @@
 		<div class="cd-scrolling-bg cd-color-2">
 			<div class="cd-container">
 				<h1 class="clr1">Archive &gt; Authors</h1>
-<!-- 				<div class="alphabet gapBelowSmall gapAboveSmall">
+					<div class="alphabet gapBelowSmall gapAboveSmall">
 					<span class="letter"><a href="authors.php?letter=A">A</a></span>
 					<span class="letter"><a href="authors.php?letter=B">B</a></span>
 					<span class="letter"><a href="authors.php?letter=C">C</a></span>
@@ -30,36 +30,44 @@
 					<span class="letter"><a href="authors.php?letter=X">X</a></span>
 					<span class="letter"><a href="authors.php?letter=Y">Y</a></span>
 					<span class="letter"><a href="authors.php?letter=Z">Z</a></span>
-				</div> -->
+					<span class="letter"><a href="authors.php?letter=Special">#</a></span>
+				</div>
 <?php
 
 include("connect.php");
 require_once("common.php");
 
-// if(isset($_GET['letter']))
-// {
-// 	$letter=$_GET['letter'];
-	
-// 	if(!(isValidLetter($letter)))
-// 	{
-// 		echo '<span class="aFeature clr2">Invalid URL</span>';
-// 		echo '</div> <!-- cd-container -->';
-// 		echo '</div> <!-- cd-scrolling-bg -->';
-// 		echo '</main> <!-- cd-main-content -->';
-// 		include("include_footer.php");
+if(isset($_GET['letter']))
+{
+	$letter=$_GET['letter'];
 
-//         exit(1);
-// 	}
-	
-// 	($letter == '') ? $letter = 'A' : $letter = $letter;
-// }
-// else
-// {
-// 	$letter = 'A';
-// }
+	if(!(isValidLetter($letter)))
+	{
+		echo '<span class="aFeature clr2">Invalid URL</span>';
+		echo '</div> <!-- cd-container -->';
+		echo '</div> <!-- cd-scrolling-bg -->';
+		echo '</main> <!-- cd-main-content -->';
+		include("include_footer.php");
 
-// $query = 'select * from author where authorname like \'' . $letter . '%\' order by authorname';
-$query = 'select * from author order by authorname';
+		exit(1);
+	}
+
+	($letter == '') ? $letter = 'A' : $letter = $letter;
+}
+else
+{
+	$letter = 'A';
+}
+
+//~ $query = 'select * from author where authorname like \'' . $letter . '%\' order by authorname';
+if($letter == 'Special')
+{
+	$query = "select * from author where authorname not regexp '^[a-z]|^\'[a-z]|^\"[a-z]|^<|^\"<' order by authorname";
+}
+else
+{
+	$query = "select * from author where authorname like '$letter%' union select * from author where authorname like '\"$letter%' union select * from author where authorname like '\'$letter%' order by TRIM(BOTH '\'' FROM TRIM(BOTH '\"' FROM authorname))";
+}
 
 $result = $db->query($query); 
 $num_rows = $result ? $result->num_rows : 0;
@@ -69,13 +77,13 @@ if($num_rows > 0)
 	while($row = $result->fetch_assoc())
 	{
 		echo '<div class="author">';
-		echo '	<span class="aAuthor"><a href="auth.php?authid=' . $row['authid'] . '&amp;author=' . urlencode($row['authorname']) . '">' . $row['authorname'] . '</a> ';
+		echo '	<span class="aAuthor"><a href="auth.php?authid=' . $row['authid'] . '&amp;author=' . urlencode($row['authorname']) . '">' . $row['authorname'] . '</a>';
 		echo '</div>';
 	}
 }
 else
 {
-	echo '<span class="sml">Sorry! No author names were found to begin with the letter \'' . $letter . '\' in The Vedanta Kesari</span>';
+	echo '<span class="sml">Sorry! No author names were found to begin with the letter \'' . $letter . '\' in Tattvaloka</span>';
 }
 
 if($result){$result->free();}
