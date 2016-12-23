d5f3a673a0d14e118d6857a1716e42c1d1cf5873
suresh07 <soori_rozee@yahoo.com>
2016-12-23 18:03:36 +0530

    No. 60 Tab icon added

--- php/include_header.php
+++ php/include_header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
+	<link rel="shortcut icon" href="img/aplogo.ico">
 	<link rel="stylesheet" href="css/font.css"> <!-- Font style -->
 	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
 	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
